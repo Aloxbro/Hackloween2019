2044d9a3220167374f6470fce3a193722bfd185a
Aloxbro <alox51@wanadoo.fr>
2019-10-30 14:09:04 +0100

    styling some stuff and fixing home and landing page for sophie component

--- hallowcine/src/components/Main.js
+++ hallowcine/src/components/Main.js
@@ -6,11 +6,13 @@ from 'react-router-dom';
 import LandingPage from './LandingPage';
 import OurSelection from './OurSelection';
 import TryMe from './TryMe';
+import Home from './Home';
 
 function Main() {
     return (
         <Switch>
             <Route exact path="/" component={LandingPage} />
+            <Route exact path="/home" component={Home} />
             <Route exact path="/our-selection" component={OurSelection} />
             <Route exact path="/try-me" component={TryMe} />
         </Switch>
