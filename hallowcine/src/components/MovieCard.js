7d140592ef48f04d34839406c35ca14f2c99f856
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-10-30 17:17:32 +0100

    cards for the movies and functional API call

--- hallowcine/src/components/MovieCard.js
+++ hallowcine/src/components/MovieCard.js
@@ -1,5 +1,7 @@
 import React from "react"
 import axios from "axios"
+import "./MovieCard.css"
+import ReactFitText from "react-fittext"
 
 export default class MovieCard extends React.Component {
 	constructor(props) {
@@ -7,34 +9,34 @@ export default class MovieCard extends React.Component {
 		this.state = {
 			getMovies: {},
 		}
-		// this.fetchingMovieListFromAPI.bind = this.fetchingMovieListFromAPI.bind(this)
 	}
 
-	fetchingMovieListFromAPI = () => {
+	componentDidMount = () => {
 		axios
 			.get("https://hackathon-wild-hackoween.herokuapp.com/movies/")
-			.then(movieList => movieList.data)
-			.then(data => this.setState({getMovies: data}))
+			.then(response => this.setState({getMovies: response.data}))
 	}
 
 	render() {
 		return (
 			<div>
-				<button onClick={this.fetchingMovieListFromAPI}>
-					Movie Button Test
-				</button>
 				{this.state.getMovies.movies ? (
-					this.state.getMovies.movies.map(
-						<div>
+					this.state.getMovies.movies.map(movie => (
+						<div className="MovieCard">
 							<img
-								src={this.state.getMovies.movies.posterUrl}
-								alt={this.state.getMovies.movies.title}
+								src={movie.posterUrl}
+								alt={movie.title}
+								className="MovieCardImage"
 							/>
-							<h2>{this.state.getMovies.movies.title}</h2>
-							<p>Réalisateur: {this.state.getMovies.movies.director} </p>
-							<p>Année: {this.state.getMovies.movies.year} </p>
-						</div>,
-					)
+							<ReactFitText compressor="0.1" maxFontSize="30px">
+								<h2 className="MovieCardTitle">{movie.title}</h2>
+							</ReactFitText>
+							<p className="MovieCardDirector">
+								Réalisateur: {movie.director}{" "}
+							</p>
+							<p className="MovieCardYear">Année: {movie.year} </p>
+						</div>
+					))
 				) : (
 					<p>Loading...</p>
 				)}
