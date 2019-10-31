388a3428134ebb12cbd376086857503ceedcfbe0
Jessy Coraboeuf <coraboeufjessy@gmail.com>
2019-10-31 01:53:03 +0100

    carousel refactor into class instead of fat arrow and hooks

--- hallowcine/src/components/Carousel.js
+++ hallowcine/src/components/Carousel.js
@@ -1,58 +1,72 @@
-import React, {useState, useEffect} from "react"
+import React/* , {useState, useEffect} */ from "react"
 import ItemsCarousel from "react-items-carousel"
 import axios from "axios"
 import ReactFitText from "react-fittext"
 
-export default () => {
-	const [activeItemIndex, setActiveItemIndex] = useState(0)
-	const chevronWidth = 40
-	const [getMovies, setGetMovies] = useState([])
+// export default () => {
 
-	useEffect(() => {
+export default class Carousel extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			activeItemIndex: 0,
+			getMovies: "",
+			chevronWidth: 40,
+		}
+	}
+	// const [activeItemIndex, setActiveItemIndex] = useState(0)
+	// const chevronWidth = 40
+	// const [getMovies, setGetMovies] = useState([])
+
+	componentDidMount() {
 		axios
 			.get("https://hackathon-wild-hackoween.herokuapp.com/movies/")
-			.then(response => setGetMovies({getMovies: response.data}))
-			.then(() => {
-				setTimeout(() => {
-					console.log(getMovies)
-				}, 1000)
-			})
-	}, [])
+			.then(response => this.setState({getMovies: response.data}))
+	}
 
-	return (
-		<div style={{padding: `0 ${chevronWidth}px`}}>
-			<ItemsCarousel
-				infiniteLoop={true}
-				requestToChangeActive={setActiveItemIndex}
-				activeItemIndex={activeItemIndex}
-				numberOfCards={3}
-				gutter={20}
-				leftChevron={<button>{"<"}</button>}
-				rightChevron={<button>{">"}</button>}
-				chevronWidth={chevronWidth}
-				outsideChevron={true}
-			>
-				{getMovies.movies ? (
-					getMovies.movies.map(movie => (
-						<div className="MovieCard">
-							<img
-								src={movie.posterUrl}
-								alt={movie.title}
-								className="MovieCardImage"
-							/>
-							<ReactFitText compressor="0.1" maxFontSize="30px">
-								<h2 className="MovieCardTitle">{movie.title}</h2>
-							</ReactFitText>
-							<p className="MovieCardDirector">
-								Réalisateur: {movie.director}{" "}
-							</p>
-							<p className="MovieCardYear">Année: {movie.year} </p>
-						</div>
-					))
-				) : (
-					<p>Loading...</p>
-				)}
-			</ItemsCarousel>
-		</div>
-	)
+	// useEffect(() => {
+	// 	axios
+	// 		.get("https://hackathon-wild-hackoween.herokuapp.com/movies/")
+	// 		.then(response => setGetMovies({getMovies: response.data}))
+	// 		)
+	// }, [])
+
+	render() {
+		return (
+			<div style={{padding: `0 ${this.state.chevronWidth}px`}}>
+				<ItemsCarousel
+					infiniteLoop={true}
+					requestToChangeActive={this.state.activeItemIndex/*this.setState({activeItemIndex: 0})*/}
+					activeItemIndex={this.state.activeItemIndex}
+					numberOfCards={4}
+					gutter={20}
+					leftChevron={<button>{"<"}</button>}
+					rightChevron={<button>{">"}</button>}
+					chevronWidth={this.state.chevronWidth}
+					outsideChevron={true}
+				>
+					{this.state.getMovies.movies ? (
+						this.state.getMovies.movies.map(movie => (
+							<div className="MovieCard">
+								<img
+									src={movie.posterUrl}
+									alt={movie.title}
+									className="MovieCardImage"
+								/>
+								<ReactFitText compressor="0.1" maxFontSize="30px">
+									<h2 className="MovieCardTitle">{movie.title}</h2>
+								</ReactFitText>
+								<p className="MovieCardDirector">
+									Réalisateur: {movie.director}{" "}
+								</p>
+								<p className="MovieCardYear">Année: {movie.year} </p>
+							</div>
+						))
+					) : (
+						<p>Loading...</p>
+					)}
+				</ItemsCarousel>
+			</div>
+		)
+	}
 }
